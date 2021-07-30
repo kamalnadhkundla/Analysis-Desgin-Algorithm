4b2e2b3b99365ab18f2012bc7aa8e5bdb73dac4a
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-07-30 11:30:26 +0530

    Create Order of Indices.py

---
+++ Lab - 3/In - Lab/Order of Indices.py
@@ -0,0 +1,25 @@
+"""
+7,6,10,5,9,2,1,15,7
+"""
+
+def partition(arr,low,high): 
+  i = ( low-1 ) 
+  pivot = arr[high]    
+
+  for j in range(low , high): 
+    if arr[j] <= pivot: 
+      i = i+1
+      arr[i],arr[j] = arr[j],arr[i] 
+
+  arr[i+1],arr[high] = arr[high],arr[i+1] 
+  return ( i+1 ) 
+def quickSort(arr,low,high): 
+  if low < high: 
+    pi = partition(arr,low,high) 
+    quickSort(arr, low, pi-1) 
+    quickSort(arr, pi+1, high) 
+
+arr = [7,6,10,5,9,2,1,15,7] 
+n = len(arr) 
+quickSort(arr,0,n-1) 
+print(arr)

9cf61d0b9f6cb450c5efafbcd5ac6077dac4a820
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-07-30 11:10:23 +0530

    Create Sort Photos.py

---
+++ Lab - 2/In - Lab/Sort Photos.py
@@ -0,0 +1,19 @@
+"""
+Input
+5
+Neil 	Katherine 	Harry 		Stefan 		Dennis
+Output
+Dennis	Harry 		Katherine 	Neil 		Stefan
+"""
+
+p=['Neil','Katheriene','Herry','Stefan','Dennis']
+b=[]
+c=[]
+for i in range(0,len(p)):
+  if(i%2==0):
+    b.append(p[i])
+  else:
+    c.append(p[i])
+b.sort()
+c.sort()
+print(b+c)
